17efba67e6c13ee3d940e1d6b3f48feef815886f
lagger <asd@mail.ru>
2024-10-24 15:22:47 +1000

    Применил патч со сложной ветки

--- GitTrain/main/urls.py
+++ GitTrain/main/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('new/', views.new, name='new')
 
 ]
