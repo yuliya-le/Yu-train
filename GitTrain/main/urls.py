16909fbcfa5c33b9a27509f0ce906004385ce872
Vostokoved <lavrav94@mail.ru>
2024-11-11 10:06:43 +1000

    Восстановление данных

--- GitTrain/main/urls.py
+++ GitTrain/main/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('new/', views.new, name='new')
 
 ]
